d9e2b6c365bdefcf5bb12cafa5dd71fae739e456
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-11-29 23:58:04 +0100

    changes on gitignore

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/HackJ/Desktop/vdtjson
+# Build directory: C:/Users/HackJ/Desktop/vdtjson
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Samples/JsonTest")
---
+++ Samples/JsonTest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/HackJ/Desktop/vdtjson/Samples/JsonTest
+# Build directory: C:/Users/HackJ/Desktop/vdtjson/Samples/JsonTest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
