e53f6a435d295e3c20e4ffa9a7e208112d47a9e3
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-12-05 23:23:06 +0100

    serializer changes

--- src/vdtjson/serializer.h
+++ src/vdtjson/serializer.h
@@ -6,82 +6,91 @@
 
 namespace json
 {
-	std::string to_string(const value&);
-	std::string to_string(const number&);
-	std::string to_string(const value::array_t&);
-	std::string to_string(const value::object_t&);
-
-	std::string to_string(const value& value)
+	class Serializer final
 	{
-		static constexpr char* quote = "\"";
+	public:
 
-		switch (value.type())
+		static std::string to_string(const value& value)
 		{
-		case value::Type::Bool:
-			return std::to_string(value.as_bool());
-		case value::Type::Number:
-			return to_string(value.as_number());
-		case value::Type::Array:
-			return to_string(value.as_array());
-		case value::Type::Object:
-			return to_string(value.as_object());
-		case value::Type::String:
-		default:
-			return std::string{quote}.append(value.as_string()).append(quote);
+			switch (value.type())
+			{
+			case value::Type::Bool:
+				return std::to_string(value.as_bool());
+			case value::Type::Number:
+				return to_string(value.as_number());
+			case value::Type::Array:
+				return to_string(value.as_array());
+			case value::Type::Object:
+				return to_string(value.as_object());
+			case value::Type::String:
+			default:
+				return std::string{ quote }.append(value.as_string()).append(quote);
+			}
 		}
-	}
 
-	std::string to_string(const number& number)
-	{
-		switch (number.type())
+		static std::string to_string(const number& number)
 		{
-		case number::Type::Int:
-			return std::to_string(number.as_int());
-		case number::Type::Float:
-			return std::to_string(number.as_float());
-		case number::Type::Double:
-		default:
-			return std::to_string(number.as_double());
+			switch (number.type())
+			{
+			case number::Type::Int:
+				return std::to_string(number.as_int());
+			case number::Type::Float:
+				return std::to_string(number.as_float());
+			case number::Type::Double:
+			default:
+				return std::to_string(number.as_double());
+			}
 		}
-	}
 
-	std::string to_string(const value::array_t& array)
-	{
+		static std::string to_string(const value::array_t& array)
+		{
+			std::string result{ "[" };
+
+			const size_t size = array.size();
+			for (int i = 0; i < size; ++i)
+			{
+				result.append(to_string(array[i]));
+				if (i < size - 1) result.append(comma);
+			}
+
+			result.append("]");
+			return result;
+		}
+
+		static std::string to_string(const value::object_t& object)
+		{
+
+			std::string next{};
+
+			std::string result{ "{" };
+
+			for (const auto& pair : object)
+			{
+				result.append(next)
+					.append(quote)
+					.append(pair.first)
+					.append(quote_equals)
+					.append(to_string(pair.second));
+				next = comma;
+			}
+
+			return result.append("}");
+		}
+
+
+	private:
+
+		Serializer() = default;
+		~Serializer() = default;
+
 		static constexpr char* comma = ",";
-
-		std::string result{ "[" };
-
-		const size_t size = array.size();
-		for (int i = 0; i < size; ++i)
-		{
-			result.append(to_string(array[i]));
-			if (i < size - 1) result.append(comma);
-		}
-
-		result.append("]");
-		return result;
-	}
-
-	std::string to_string(const value::object_t& object)
-	{
-		static constexpr char* comma = ",";
+		static constexpr char* equals = ":";
 		static constexpr char* quote = "\"";
-		static constexpr char* equals = "\":";
+		static constexpr char* quote_equals = "\":";
+	};
 
-		std::string next{};
-
-		std::string result{ "{" };
-
-		for (const auto& pair : object)
-		{
-			result.append(next)
-				.append(quote)
-				.append(pair.first)
-				.append(equals)
-				.append(to_string(pair.second));
-			next = comma;
-		}
-
-		return result.append("}");
-	}
+	std::string to_string(const value& value) { return Serializer::to_string(value); }
+	std::string to_string(const number& value) { return Serializer::to_string(value); }
+	std::string to_string(const value::array_t& value) { return Serializer::to_string(value); }
+	std::string to_string(const value::object_t& value) { return Serializer::to_string(value); }
 }
